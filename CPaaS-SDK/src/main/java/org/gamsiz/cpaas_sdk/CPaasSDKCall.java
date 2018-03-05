afec79faf3f0fc8e07687c7b1454590a16c764ed
Mustafa Gamsiz <mgamsiz@netas.com.tr>
2018-03-05 15:23:07 +0300

    CPaaSSDKProject init.

---
+++ CPaaS-SDK/src/main/java/org/gamsiz/cpaas_sdk/CPaasSDKCall.java
@@ -0,0 +1,15 @@
+package org.gamsiz.cpaas_sdk;
+
+import android.util.Log;
+
+/**
+ * Created by mgamsiz on 05/03/2018.
+ */
+
+public class CPaasSDKCall {
+
+    public CPaasSDKCall() {
+
+    }
+
+}
