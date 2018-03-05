ec48dacbb3bfa5b1e313d5645445f4e1d161cba3
Mustafa Gamsiz <mgamsiz@netas.com.tr>
2018-03-05 15:42:48 +0300

    WebRTC version 2 usage.

--- CPaaS-SDK/src/main/java/org/gamsiz/cpaas_sdk/CPaasSDKCall.java
+++ CPaaS-SDK/src/main/java/org/gamsiz/cpaas_sdk/CPaasSDKCall.java
@@ -2,14 +2,28 @@ package org.gamsiz.cpaas_sdk;
 
 import android.util.Log;
 
+import org.gamsiz.webrtc.WebRTCCall;
+
 /**
  * Created by mgamsiz on 05/03/2018.
  */
 
 public class CPaasSDKCall {
 
-    public CPaasSDKCall() {
+    private WebRTCCall webRTCCall;
 
+    public CPaasSDKCall() {
+        this.webRTCCall = new WebRTCCall();
+    }
+
+    public void establishCall() {
+        webRTCCall.establishCall();
+        Log.i("CPaasSDKCall v1", "CPaasSDKCall-v1 established");
+    }
+
+    public void endCall() {
+        webRTCCall.endCall();
+        Log.i("CPaasSDKCall v1", "CPaasSDKCall-v1 established");
     }
 
 }
